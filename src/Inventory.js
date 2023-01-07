d9d10a5a89eecc8cdc29bdf1e7d12bbaa8a9d9f5
Jacob <yoqub.xamidoff19@gmail.com>
2023-01-07 18:34:36 +0900

    1
    1

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,13 +1,99 @@
-import { Typography } from 'antd';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Modal, Form, Input } from 'antd';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-const { Title } = Typography;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEsm2uX4Ott4vxlH-K_p25xnYPShXv6FI",
+  authDomain: "biflow-efa49.firebaseapp.com",
+  databaseURL: "https://biflow-efa49-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "biflow-efa49",
+  storageBucket: "biflow-efa49.appspot.com",
+  messagingSenderId: "257343919180",
+  appId: "1:257343919180:web:ffaa88ed28958ff04c90e9",
+  measurementId: "G-89DK3S524X"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+const { Column } = Table;
+
 function Inventory() {
-   return (
-      <div>
-         <Title>Inventory test 3</Title>
-      </div>
-   );
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection('inventory')
+      .onSnapshot((snapshot) => {
+        const items = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(items);
+      });
+    return unsubscribe;
+  }, []);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleCreate = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        form.resetFields();
+        firebase
+          .firestore()
+          .collection('items')
+          .add(values);
+        setVisible(false);
+      })
+      .catch((info) => {
+        console.log('Validate Failed:', info);
+      });
+  };
+
+  return (
+    <div>
+      <Button type="primary" onClick={showModal}>
+        Add Item
+      </Button>
+      <Table dataSource={items} rowKey="id">
+        <Column title="Name" dataIndex="name" key="name" />
+        <Column title="Quantity" dataIndex="quantity" key="quantity" />
+        <Column title="Price" dataIndex="price" key="price" />
+      </Table>
+      <Modal
+        title="Add Item"
+        visible={visible}
+        onCancel={handleCancel}
+        onOk={handleCreate}
+      >
+        <Form form={form}>
+          <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="quantity" label="Quantity" rules={[{ required: true }]}>
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item name="price" label="Price" rules={[{ required: true }]}>
+            <Input type="number" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
 }
 
 export default Inventory;
