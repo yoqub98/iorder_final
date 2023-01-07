d2a0d347f8ac576044c11971fc6b04d630ec2139
Bubble Ninja <mr.slygrin@gmail.com>
2023-01-07 14:03:40 +0500

    test push

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { Typography} from 'antd';
+import { Typography } from 'antd';
 
 const { Title } = Typography;
-function Inventory () {
-    return (
-    <div>
-   <Title>Inventory test</Title>
-   </div>
-  )
+function Inventory() {
+   return (
+      <div>
+         <Title>Inventory test 3</Title>
+      </div>
+   );
 }
 
-export default Inventory ;
+export default Inventory;
