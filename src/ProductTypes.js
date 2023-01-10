a1ab2217dba82ac485cdf8458ea5bb88a5c6e5e4
Jacob <yoqub.xamidoff19@gmail.com>
2023-01-11 02:11:17 +0900

    delete tovar added
    1

--- src/ProductTypes.js
+++ src/ProductTypes.js
@@ -1,14 +1,56 @@
-import { Card, Col, Row, } from 'antd';
-import './product_card_styling.css';
+import { DeleteOutlined, InfoCircleOutlined, EditOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+import { Card, Col, Row, Modal, message } from 'antd';
+import { initializeApp } from "firebase/app";
+import { Spin } from 'antd';
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import {deleteDoc, doc, getFirestore,} from "firebase/firestore";
+
 const { Meta } = Card;
 const Product_types = (props) => {
   const { data } = props;
+  const [visible, setVisible] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
+
+  const showDeleteConfirm = (product) => {
+    Modal.confirm({
+      title: 'Удалить',
+      content: 'Удалить данный товар с базы данных?',
+      okText: 'Да',
+      okType: 'danger',
+      cancelText: 'Нет',
+      onOk() {
+        deleteProduct(product);
+      },
+    });
+  };
+
+  const deleteProduct = async (product) => {
+    const db = getFirestore();
+    const docRef = doc(db, "products", product.id);
+    deleteDoc(docRef)
+      .then(() => {
+        message.success('Товар успешно удален с базы данных!');
+             
+      })
+      .catch((error) => {
+        message.error('Ошибка при удалении: ' + error.message);
+      });
+  };
 
   return (
     <Row gutter={16}>
       {data.map((item) => (
         <Col key={item.id} span={8} >
-          <Card style={{ marginBottom: '18px' }} bordered={false} headStyle={{ borderColor: '#1890ff' , borderWidth: 1.5, backgroundColor: '#fafafa'}} type="inner" title={item.name}>
+          <Card  style={{ marginBottom: '18px' }} bordered={false} headStyle={{ borderColor: '#1890ff' , borderWidth: 1.5, backgroundColor: '#fafafa'}} 
+          type="inner" title={item.name} 
+          actions={[
+            <InfoCircleOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <DeleteOutlined key="ellipsis" onClick={() => showDeleteConfirm(item)} />,
+          ]}>
            Цена : {item.price} сум/шт
            <Meta style={{ marginTop: '12px' }} description={"Тип упаковки : " + " " + item.type}>
            
