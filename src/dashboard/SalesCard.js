91f5ce89f6b3ec48fe109544fc9e4848a0a6e2b2
Jacob <yoqub.xamidoff19@gmail.com>
2023-01-12 21:33:48 +0900

    dashboard
    UI

--- src/dashboard/SalesCard.js
+++ src/dashboard/SalesCard.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from 'antd';
+import { Card , Statistic} from 'antd';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEsm2uX4Ott4vxlH-K_p25xnYPShXv6FI",
@@ -20,65 +20,70 @@ var db = firebase.firestore();
 
 
 const SalesCard = () => {
-   // Get current and previous month in the format 'YYYY-MM'
-   const current = new Date();
-const currentMonth = current.toISOString().slice(0, 7);
-let previousMonth = new Date(current);
-previousMonth.setUTCMonth(current.getUTCMonth()-1);
-previousMonth = previousMonth.toISOString().slice(0, 7);
+  const [currentMonthSales, setCurrentMonthSales] = useState(0);
+  const [previousMonthSales, setPreviousMonthSales] = useState(0);
+
+  useEffect(() => {
+     // Get current month and previous month
+     var currentYear = new Date().getFullYear();
+     var currentMonth = new Date().getMonth();
+     var previousMonth;
+     var previousYear;
+     if (currentMonth === 0) {
+         previousMonth = 11;
+         previousYear = currentYear - 1;
+     } else {
+         previousMonth = currentMonth - 1;
+         previousYear = currentYear;
+     }
  
-   // Get total sales for current month and previous month
-   const [currentSales, setCurrentSales] = useState(0);
-   const [previousSales, setPreviousSales] = useState(0);
+     var startOfCurrentMonth = new Date(currentYear, currentMonth, 1);
+     var startOfPreviousMonth = new Date(previousYear, previousMonth, 1);
+     var endOfPreviousMonth = new Date(previousYear, previousMonth, new Date(previousYear, previousMonth+1, 0).getDate());
  
-   useEffect(() => {
-     db.collection('orders')
-       .where('date', '>=', `${currentMonth}-01T00:00:00`)
-       .where('date', '<=', `${currentMonth}-31T23:59:59`)
-       .get()
-       .then(snapshot => {
-         let total = 0;
-         snapshot.forEach(doc => {
-           total += doc.data().total;
-         });
-         setCurrentSales(total);
-       })
-       .catch(error => {
-         console.error('Error getting current month sales: ', error);
-       });
-   }, [ [currentMonth]]);
+     var currentMonthRef = db.collection("orders").where("date", ">=", firebase.firestore.Timestamp.fromDate(startOfCurrentMonth));
+     var previousMonthRef = db.collection("orders").where("date", ">=", firebase.firestore.Timestamp.fromDate(startOfPreviousMonth)).where("date","<=", firebase.firestore.Timestamp.fromDate(endOfPreviousMonth));
  
-   useEffect(() => {
-    let total = 0;
-    db.collection('orders')
-       .where('date', '>=', `${previousMonth}-01T00:00:00`)
-       .where('date', '<=', `${previousMonth}-31T23:59:59`)
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          total += doc.data().total;
-        });
-        setPreviousSales(total);
-      })
-      .catch(error => {
-        console.error('Error getting previous month sales: ', error);
-      });
+     // Get sales data for current month
+     currentMonthRef.get().then(function(querySnapshot) {
+     var total = 0;
+     querySnapshot.forEach(function(doc) {
+         total += doc.data().total;
+     });
+     setCurrentMonthSales(total);
+     });
+ 
+     // Get sales data for previous month
+     previousMonthRef.get().then(function(querySnapshot) {
+     var total = 0;
+     querySnapshot.forEach(function(doc) {
+         total += doc.data().total;
+     });
+     setPreviousMonthSales(total);
+     });
   }, []);
- 
-   // Calculate the sales growth compared to previous month
-   let salesGrowth;
-   if (previousSales === 0) {
-     salesGrowth = 'NA';
-   } else {
-     salesGrowth = ((currentSales - previousSales) / previousSales) * 100;
-   }
- 
-   return (
-     <Card>
-       <p>Current Month Sales: {currentSales}</p>
-       <p>Previous Month Sales: {previousSales}</p>
-       <p>Sales Growth: {salesGrowth}%</p>
-     </Card>
-   );
-}
+
+  // Calculate sales growth
+  let salesGrowth = ((currentMonthSales - previousMonthSales) / previousMonthSales * 100).toFixed(2)
+  
+  if(isNaN(salesGrowth)){
+      salesGrowth = 0;
+  }
+  return (
+      <Card title="Обьем продаж" bordered={false}  >
+       <Statistic style={{fontWeight : "600"}} title="Этот месяц:" value={currentMonthSales.toLocaleString("en-US")+" сум"}  />
+       <Statistic style={{fontWeight : "300", fontSize: 8}}
+          
+            value={salesGrowth + "%"}
+            precision={2}
+            valueStyle={{ color: '#3f8600' }}
+            prefix={<ArrowUpOutlined size={2} />}
+            suffix="%"
+          />
+          <p>Прошлый месяц: {previousMonthSales.toLocaleString("en-US")+" сум"}</p>
+          
+      </Card>
+  );
+};
+
 export default SalesCard;
